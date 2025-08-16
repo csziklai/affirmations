6b1c8ff522e559c2e3a5ab3dc1d963a610e1928f
Cassandra Sziklai <cns58@cornell.edu>
2025-08-15 22:23:27 -0400

    Barebones home screen layout

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,11 @@
 import { ImageBackground } from "expo-image";
-import { StyleSheet, View } from "react-native";
-//import backgroungImg from '../assets/images/aura_gradient.png';
+import { StyleSheet, View, Text } from "react-native";
+
+import Icon from 'react-native-vector-icons/Feather';
+// import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+// import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+// import FavoriteIcon from '@mui/icons-material/Favorite';
+// import CategoryIcon from '@mui/icons-material/Category';
 
 export default function Index() {
   return (
@@ -8,11 +13,19 @@ export default function Index() {
       style={styles.container}
     >
       <ImageBackground
-        source={require('@/assets/images/aura_gradient.png')}
-        resizeMode="cover"
+        source={require('../assets/images/aura_gradient.jpeg')}
+        contentFit="cover"
         style={styles.image}>
+          <Text style={styles.text}
+          >I am brave enough to make changes that will bring me happiness.</Text>
+
+          <View style={styles.icons}>
+            <Icon name="upload" size={30} color="d9d9d9" style={{ marginRight: 50 }} />
+            <Icon name="heart" size={30} color="d9d9d9"/>
+          </View>
 
       </ImageBackground>
+
 
     </View>
   );
@@ -30,5 +43,17 @@ const styles = StyleSheet.create({
     flex: 1,
     resizeMode: 'cover',
     justifyContent: 'center',
+  },
+  icons: {
+    flexDirection:'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 50
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: 20,
+    marginHorizontal: 30,
+    fontFamily: 'Lora-Regular',
   }
 });
